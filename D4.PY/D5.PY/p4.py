f3be90cdbfb598725b44dd7955d24e2341a28d8c
Deepti <deeptigarkoti9@gmail.com>
2026-02-08 17:03:10 +0530

    day 5

---
+++ D4.PY/D5.PY/p4.py
@@ -0,0 +1,10 @@
+# WHILE LOOP ( RUNS TILL A CONDITION IS TRUE)
+#BEFORE EVERY ITERATION , IT CHECKS IF THE CONDITION IS STILL TRUE.
+'''FIRST INITIALISE WITH ->    i=0
+then use while loop ->         while i<10:
+then code  and at last update the elements -> i+=1'''
+#EXAMPLE: 
+i=2
+while i<21:
+    print(i)
+    i+=2
